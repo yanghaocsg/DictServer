414cc1acece0bfdf6d58cf6e36e391d3cc98c2fc
yanghaocsg <287546756@qq.com>
2015-02-13 20:05:55 +0800

    DictConf.py

--- DictConf.py
+++ DictConf.py
@@ -13,78 +13,76 @@ from tornado.ioloop import IOLoop
 import tornado.gen, tornado.web
 import ConfigParser
 import eventlet
-import threading
+import threading, glob
 
 #self module
 import YhLog, YhTool
 logger = logging.getLogger(__file__)
 
 class DictManager:
-    def __init__(self, conf='./conf/dict.conf', password=''):
+    def __init__(self, conf='./conf/dict.conf'):
         self.cwd = Path(__file__).absolute().ancestor(1)
-        self.config = ConfigParser.ConfigParser()
-        self.config.read(Path(self.cwd, conf))
+        self.conf = conf
         self.dict_manager = {}
         self.load()
         
     def load(self):
+        self.config=ConfigParser.ConfigParser({'type':'unigram', 'fn_txt':'', 'fn_pic':''})
+        self.config.read(Path(self.cwd, self.conf))
         for sec in self.config.sections():
-            dict_tmp = {}
+            dict_type = self.config.get(sec, 'type')
+            txt = self.config.get(sec, 'fn_txt')
+            pic = self.config.get(sec, 'fn_pic')
+            self.dict_manager[sec] = self.load_dict(dict_type, txt, pic)
+            logger.error('init name %s len %s type %s data %s' % (sec, len(self.dict_manager[sec]), self.config.get(sec, 'type'), self.dict_manager[sec].items()[:3]))
             
-            try:
-                dict_type = self.config.get(sec, 'type')
-                if  dict_type in ['unigram']:
-                    logger.error('before load_unigram')
-                    dict_tmp = self.load_unigram(self.config.get(sec, 'fn_txt'))
-                elif dict_type in ['bigram']:
-                    logger.error('before load_bigram')
-                    dict_tmp = self.load_bigram(self.config.get(sec, 'fn_txt'))
-                else:
-                    logger.error('before nochoice')
-                logger.error('sec [%s] type [%s]' % (sec, dict_type))
-            except:
-                logger.error('DictConf error %s' % traceback.format_exc())
-            self.dict_manager[sec] = dict_tmp
-            logger.error('init name %s len %s type %s' % (sec, len(self.dict_manager[sec]), self.config.get(sec, 'type')))
-            
-    def load_unigram(self, ifn_txt=''):
+    def load_dict(self, dict_type='unigram', fn_txt='', fn_pic=''):
+        if dict_type not in ['unigram', 'bigram']:
+            raise Exception('dict type error')
         dict_tmp = {}
-        for l in open(Path(self.cwd, ifn_txt)):
-            l = unicode(l.strip(), 'utf8', 'ignore')
-            if not l: continue
-            dict_tmp[l] = 1
-        logger.error('load_unigram %s %s' % (ifn_txt, len(dict_tmp)))
+        if fn_pic:
+            for f in glob.glob(Path(self.cwd, fn_pic)):
+                d = cPickle.load(open(f))
+                dict_tmp.update(d)
+                logger.error('load_dict %s %s %s' % (f, len(d), len(dict_tmp)))
+        else:
+            for f in glob.glob(Path(self.cwd, fn_txt)):
+                d = {}
+                for l in open(f):
+                    l = unicode(l.strip(), 'utf8', 'ignore')
+                    if not l: continue
+                    if dict_type == 'bigram':
+                        k, v = re.split('\t', l, 2)
+                        v = int(v)
+                        if k and v:
+                            d[k] = v
+                    else:
+                        d[l] = 1
+                dict_tmp.update(d)
+                logger.error('load_dict %s %s %s' % (f, len(d), len(dict_tmp)))
         return dict_tmp
-    
-    def load_bigram(self, ifn_txt=''):
-        dict_tmp = {}
-        for l in open(Path(self.cwd, ifn_txt)):
-            try:
-                l = unicode(l.strip(), 'utf8', 'ignore')
-                if not l: continue
-                k,v = l.split('\t')[:2]
-                dict_tmp[k] = int(v)
-            except:
-                #logger.error('load_bigram %s %s' % (l, traceback.format_exc()))
-                pass
-        logger.error('load_bigram %s %s' % (ifn_txt, len(dict_tmp)))
-        return dict_tmp
-    
+        
     def get_query(self, req={}):
         name = req.get('dict', '')
         query = req.get('query', '')
         dict_res = {}
-        if sec and query and sec in self.dict_manager:
+        logger.error('get_query %s\t%s\t%s' % (name, query, name in self.dict_manager))
+        if name and query and name in self.dict_manager:
             try:
                 if isinstance(query, list):
                     for q in query:
                         val = self.dict_manager[name].get(q,0)
                         if val:
                             dict_res[q] = val
+                        else:
+                            logger.error('not found %s' % q)
+                            
                 else:
                     val = self.dict_manager[name].get(query,0)
                     if val:
                         dict_res[query] = val
+                    else:
+                        logger.error('not found %s' % val)
             except:
                 logger.error('DictManager %s' % traceback.format_exc())
         return dict_res
@@ -97,11 +95,12 @@ def reload():
     if idx_manager_lock:
         #already reloading
         return 2  
-    idx_manager_lock = 1
+    idx_manager_lock = 1 
     idx_manager_backup = (idx_manager + 1) % 2
     manager[idx_manager_backup] = DictManager()
     manager[idx_manager] = {}
     idx_manager = idx_manager_backup
+    
     idx_manager_lock = 0
     print "reload end"
     return idx_manager
@@ -133,11 +132,12 @@ class Search_Handler(tornado.web.RequestHandler):
             dict_qs = YhTool.yh_urlparse_params(self.request.uri, ['req'], [''])
             query = dict_qs['req']
             dict_req = simplejson.loads(query)
-            dict_res = manager[idx_manager].get(dict_req)
+            logger.error(dict_req)
+            dict_res = manager[idx_manager].get_query(dict_req)
             self.set_header('Content-Type', 'application/json; charset=UTF-8')
             self.write(simplejson.dumps(dict_res))
         except:
-            logger.error('search handler fail')
+            logger.error('search handler fail %s' % traceback.format_exc())
             self.write({'status':-1, 'res':traceback.format_exc()})
         finally:
             self.finish()
--- TestDict.py
+++ TestDict.py
@@ -15,7 +15,14 @@ logger = logging.getLogger(__file__)
 
 def test():
     dict_req = {'dict':'keyword', 'query':['abc', '电影', '电视', '二炮手']}
-    url_req = 'http://127.0.0.1:8888/se?req=%s' % (urllib.quote_plus(simplejson.dumps(dict_req)))
+    str_req = simplejson.dumps(dict_req)
+    url_req = 'http://127.0.0.1:8889/se?req=%s' % (urllib.quote_plus(str_req))
+    logger.error(url_req)
+    res = urllib2.urlopen(url_req).read()
+    logger.error(simplejson.loads(res))
+    
+    dict_req = {'dict':'unigram_rank', 'query':['9437190', '8388615', '电视', '6291465']}
+    url_req = 'http://127.0.0.1:8889/se?req=%s' % (urllib.quote_plus(simplejson.dumps(dict_req)))
     logger.error(url_req)
     res = urllib2.urlopen(url_req).read()
     logger.error(simplejson.loads(res))
